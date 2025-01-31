519a25fb800feb880b861c026ca6efc5f083ac48
Jordy <jordyl.reyesl@gmail.com>
2025-01-31 11:09:50 +0100

    Changes

--- Trimestre2/React/hola-mundo/src/App.js
+++ Trimestre2/React/hola-mundo/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Acercade from './components/Acercade.js';
 import Bucle from './components/Bucle.js';
 import Saludar from './components/Saludar.js';
+import EjemploEstado from './components/State/EjemploState.js';
+import EjemploState3 from './components/State/EjemploState3.js';
 
 function App() {
   const nombre = 'Jordy';
@@ -10,12 +12,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        {/* <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
-        <h1>Hola, {nombre}</h1>
-        <Bucle />
-        <Saludar nombre="Jordy" edad="21"/>
+        </a> */}
+        {/* <h1>Hola, {nombre}</h1> */}
+        {/* <Bucle /> */}
+        {/* <Saludar nombre="Jordy" edad="21"/> */}
+        {/* <EjemploEstado/> */}
+        <EjemploState3/>
       </header>
       <Acercade />
     </div>
