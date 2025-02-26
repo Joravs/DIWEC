7e075c4ae9faa92d8fcc4c3202a8ed0a651bcf7c
Joravs <jordyl.reyesl@gmail.com>
2025-02-26 20:46:25 +0100

    Changes

---
+++ Trimestre2/gestorweb/src/pages/Home.js
@@ -0,0 +1,10 @@
+import {Link} from 'react-router-dom';
+const Home = () => {
+    return (
+        <div>
+            <h1>Bienvenido</h1>
+            <Link to="/Pag1">Entrar</Link>
+        </div>
+    );
+};
+export default Home;
---
+++ Trimestre2/gestorweb/src/pages/Pag1.js
@@ -0,0 +1,4 @@
+const Pag1 =()=>{
+    return <>Pagina 1</>
+};
+export default Pag1;
